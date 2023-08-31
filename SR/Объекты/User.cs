b0c670d34c3440b5ab923d1c8f9bbeb6ed109920
Захар <126264978+zaharlol@users.noreply.github.com>
2023-08-31 17:34:16 +0300

    Add files via upload

---
+++ SR/Объекты/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SR
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ SR/Репозитории/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SR;
+
+namespace SR
+{
+	public class BookRepository
+	{
+        AppCont db = new AppCont();
+
+        public void Select()
+        {
+
+        }
+
+        public void Add(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        public void Delete(Book book)
+        {
+            db.Books.Remove(book);
+            db.SaveChanges();
+        }
+    }
+}
+
+
