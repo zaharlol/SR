1ea907d76cec331b7e55e97d76e82931a99c65e4
Захар <126264978+zaharlol@users.noreply.github.com>
2023-08-31 17:36:22 +0300

    Add files via upload

---
+++ EF/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        // Объекты таблицы Companies
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EF;User Id=SA;Password=reallyStrongPwd123;TrustServerCertificate=true");
+        }
+    }
+}
+
---
+++ EF/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EF
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Навигационное свойство
+        public List<User> Users { get; set; } = new List<User>();
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; }
+        public string Role { get; set; }
+
+        // Внешний ключ
+        public int CompanyId { get; set; }
+        // Навигационное свойство
+        public Company Company { get; set; }
+    }
+}
+
